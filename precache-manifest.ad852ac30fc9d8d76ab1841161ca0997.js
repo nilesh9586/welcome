8d9b242891ca39a8e69194f27278a32733b9e351
nilesh9586 <nilesh16mcg34@email.com>
2020-12-11 17:12:03 +0530

    Updates

---
+++ precache-manifest.ad852ac30fc9d8d76ab1841161ca0997.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9abf166ab0c300cab54d",
+    "url": "/welcome/static/css/main.45dc6f10.chunk.css"
+  },
+  {
+    "revision": "9abf166ab0c300cab54d",
+    "url": "/welcome/static/js/main.b398ee90.chunk.js"
+  },
+  {
+    "revision": "2c8cc3506e5cbcd658fc",
+    "url": "/welcome/static/js/runtime~main.82f1d66e.js"
+  },
+  {
+    "revision": "0fe65da901b469008e98",
+    "url": "/welcome/static/css/2.92de65d5.chunk.css"
+  },
+  {
+    "revision": "0fe65da901b469008e98",
+    "url": "/welcome/static/js/2.9f4f6707.chunk.js"
+  },
+  {
+    "revision": "f2d8383e15ab458137e41844a2dc6369",
+    "url": "/welcome/static/media/d1.f2d8383e.webp"
+  },
+  {
+    "revision": "0d9062a4823ef182867bba93cfdb287e",
+    "url": "/welcome/static/media/app.0d9062a4.jpeg"
+  },
+  {
+    "revision": "ba1bcca5ed4093fa8d3f425eac1f8b09",
+    "url": "/welcome/static/media/home.ba1bcca5.jpeg"
+  },
+  {
+    "revision": "1786ebea6164c1edefcef7c6043dae1b",
+    "url": "/welcome/static/media/ad1.1786ebea.webp"
+  },
+  {
+    "revision": "90ea2ace29dd6ec0a5ce9c5b8748b4aa",
+    "url": "/welcome/static/media/s1.90ea2ace.jpeg"
+  },
+  {
+    "revision": "1d07d85adc1fdbdb9afe49687923a1c3",
+    "url": "/welcome/static/media/m1.1d07d85a.webp"
+  },
+  {
+    "revision": "bbf7410a3cc969149e6b07983db49f14",
+    "url": "/welcome/index.html"
+  }
+];
